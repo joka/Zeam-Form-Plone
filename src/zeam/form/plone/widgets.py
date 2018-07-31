febf8db702d06c4adbddad13c1a5bffa282792e0
Joscha Krutzki <joka@jokasis.de>
2018-07-31 22:47:34 +0200

    add MultiGenericField

--- src/zeam/form/plone/widgets.py
+++ src/zeam/form/plone/widgets.py
@@ -5,12 +5,13 @@ from five import grok
 from zeam.form.base.widgets import FieldWidget
 from zeam.form.ztk.widgets.choice import ChoiceField
 from zeam.form.ztk.widgets.collection import (
-    CollectionField, newCollectionWidgetFactory, MultiChoiceFieldWidget)
+    CollectionField, newCollectionWidgetFactory, MultiGenericFieldWidget,
+    MultiChoiceFieldWidget)
 from zeam.form.ztk.widgets.text import TextField
 from zeam.form.base.interfaces import IWidget
 from zeam.form.ztk.interfaces import ICollectionField
 from zope.interface import Interface
-
+from Products.CMFPlone.resources import add_bundle_on_request, add_resource_on_request
 
 
 class PloneFieldWidget(FieldWidget, Acquisition.Explicit):
@@ -37,3 +38,19 @@ grok.global_adapter(
 class PloneMultiSelectFieldWidget(MultiChoiceFieldWidget, PloneFieldWidget):
     grok.adapts(CollectionField, ChoiceField, Interface, Interface)
     grok.name('plone.multi-select')
+
+    def update(self):
+        super(PloneMultiSelectFieldWidget, self).update()
+
+
+generic_update = MultiGenericFieldWidget.update
+
+def update_include(inst):
+    generic_update(inst)
+    add_resource_on_request(inst.request, 'json-template')
+    add_resource_on_request(inst.request, 'reordering')
+    add_resource_on_request(inst.request, 'zeam_widgets')
+    
+    
+MultiGenericFieldWidget.update = update_include
+    
